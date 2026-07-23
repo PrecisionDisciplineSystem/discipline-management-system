9697c10caf9c883dc383f7bdd8e9c71458b74eee
PrecisionDisciplineSystem <abaugher@precisionvs.com>
2026-07-23 17:54:39 -0500

    Create disciplineData.js

---
+++ assets/disciplineData.js
@@ -0,0 +1,47 @@
+const employees = [
+
+    {
+        name: "Test Employee",
+        employeeNumber: "12345",
+        location: "Main Location"
+    },
+
+    {
+        name: "John Smith",
+        employeeNumber: "54321",
+        location: "Main Location"
+    }
+
+];
+
+
+const articles = [
+
+    {
+        code: "40-2(C)",
+        description:
+        "Excessive absenteeism where notice is given (after meeting with employee) meeting the 3 in 45 threshold.",
+        
+        progressions:
+        [
+            "Reprimand",
+            "1 Week Layoff",
+            "Subject to Discharge"
+        ]
+    },
+
+
+    {
+        code: "40-3(C)",
+        description:
+        "Discourtesy to Customers.",
+        
+        progressions:
+        [
+            "Reprimand",
+            "3-Day Layoff",
+            "Subject to Discharge"
+        ]
+    }
+
+];
